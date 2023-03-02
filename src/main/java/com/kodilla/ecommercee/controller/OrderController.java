7ce5477dd5273e94a36c7dedc68c11a9bb542764
szmuraj <przemyslaw.sicinski@o2.pl>
2023-03-02 18:57:37 +0100

    Wstępna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,5 +1,40 @@
 package com.kodilla.ecommercee.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.kodilla.ecommercee.domain.OrderDTO;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class OrderController {
 
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> getOrders() {
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+
+    @GetMapping(value = "{orderId}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable Long orderId) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<OrderDTO> deleteTask(@RequestBody OrderDTO orderDTO) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping
+    public ResponseEntity<OrderDTO> updateTask(@RequestBody OrderDTO orderDTO) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createTask(@RequestBody OrderDTO orderDTO) {
+        return ResponseEntity.ok().build();
+    }
 }
