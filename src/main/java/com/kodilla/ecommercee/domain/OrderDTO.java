9328754104b329b43bdc37b781b35f7a3f6cd99b
szmuraj <przemyslaw.sicinski@o2.pl>
2023-03-05 01:54:22 +0100

    JDP230301-4 wersja po poprawkach

--- src/main/java/com/kodilla/ecommercee/domain/OrderDTO.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDTO.java
@@ -8,10 +8,4 @@ import java.text.SimpleDateFormat;
 @Getter
 @AllArgsConstructor
 public class OrderDTO {
-    private Long orderId;
-    private Long productId;
-    private double productAmount;
-    private SimpleDateFormat orderDate;
-    private SimpleDateFormat deliveryDate;
-    private String address;
 }
