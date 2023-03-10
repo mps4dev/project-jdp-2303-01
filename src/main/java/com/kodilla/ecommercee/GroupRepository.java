527efb0194d350d37554d8a3a8b887caffa6157e
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-10 15:27:53 +0100

    final commit

--- src/main/java/com/kodilla/ecommercee/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.ecommercee;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends CrudRepository<Group,Long> {
-}
