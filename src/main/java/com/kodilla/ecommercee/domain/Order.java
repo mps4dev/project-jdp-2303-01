420b9e27f37074eed036fd13afb0faa130833f85
szmuraj <przemyslaw.sicinski@o2.pl>
2023-03-16 11:30:01 +0100

    implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -5,19 +5,37 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Entity
-@Table(name = "orders")
+@Table(name = "products")
 public class Order {
-
     @Id
     @GeneratedValue
     private Long orderId;
 
-    @ManyToOne
-    @NotNull
-    @JoinColumn(name = "user_id")
-    private User user;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "join_orders_carts",
+            joinColumns = {@JoinColumn(name = "order_id")},
+            inverseJoinColumns = {@JoinColumn(name = "cart_id")})
+    private List<Cart> carts = new ArrayList<>();
+
+    @Column(name = "orderDate")
+    private SimpleDateFormat orderDate;
+
+    @Column(name = "orderPaymentDate")
+    private SimpleDateFormat orderPaymentDate;
+
+    @Column(name = "orderValue")
+    private int orderValue;
+
+    @Column(name = "orderAmountPaid")
+    private int orderAmountPaid;
+
+
 }
