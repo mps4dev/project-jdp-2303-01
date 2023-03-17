2a9f625db4733c6ebed5a1dbad10ed191a9fae12
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-17 15:15:33 +0100

    JDP230301-22 GroupController - rzeczywista implementacja metod

--- src/main/java/com/kodilla/ecommercee/domain/dto/GroupDTO.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDTO.java
@@ -1,4 +1,18 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import com.kodilla.ecommercee.domain.Product;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class GroupDTO {
+    private Long groupId;
+    private String name;
+    List<Product> products = new ArrayList<>();
+    public GroupDTO(String name, List<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
 }
