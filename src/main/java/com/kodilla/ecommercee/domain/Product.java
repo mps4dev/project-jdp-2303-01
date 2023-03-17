02c7d7329bac8f43b30e9633bbf264f2b36eac54
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-03-17 23:35:08 +0100

    reparing commit

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -5,16 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product,Long> {
     List<Product> findAll();
     }
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findAll();
-    Optional<Product> findById(Long id);
-    Product save(Product product);
-    void delete(Product product);
-}
