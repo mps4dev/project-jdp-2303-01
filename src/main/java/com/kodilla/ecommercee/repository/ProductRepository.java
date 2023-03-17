7c90419b1882e5ab7315ada18408ba664b32a80c
domKul <114921142+domKul@users.noreply.github.com>
2023-03-17 22:57:45 +0100

    Update ProductRepository.java

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product,Long> {
-    List<Product> findAll()
+    List<Product> findAll();
     }
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
