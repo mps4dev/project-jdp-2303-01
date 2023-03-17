3725798dbdcea50526a28e57acbf38ab8469815c
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-17 19:01:49 +0100

    dodane id do Product i Group

--- src/main/java/com/kodilla/ecommercee/domain/dto/GroupDTO.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDTO.java
@@ -1,18 +1,17 @@
 package com.kodilla.ecommercee.domain.dto;
 
 import com.kodilla.ecommercee.domain.Product;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class GroupDTO {
     private Long groupId;
     private String name;
     List<Product> products = new ArrayList<>();
-    public GroupDTO(String name, List<Product> products) {
-        this.name = name;
-        this.products = products;
-    }
+
 }
--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -10,11 +10,15 @@ import java.util.stream.Collectors;
 @Service
 public class GroupMapper {
     public Group mapToGroup(GroupDTO groupDTO) {
-        return new Group(groupDTO.getName(), groupDTO.getProducts());
+        return new Group(groupDTO.getGroupId(),
+                groupDTO.getName(),
+                groupDTO.getProducts());
     }
 
     public GroupDTO mapToGroupDTO(Group group) {
-        return new GroupDTO(group.getName(), group.getProducts());
+        return new GroupDTO(group.getGroupId(),
+                group.getName(),
+                group.getProducts());
     }
 
     public List<GroupDTO> mapToGroupDTOList(List<Group> groupList) {
