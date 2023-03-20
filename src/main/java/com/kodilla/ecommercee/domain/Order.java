604eae6a480da5e4a43896fa1a87948b693fa881
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-20 22:43:59 +0100

    poprawki do encji Order

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,14 +1,13 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Entity
@@ -18,30 +17,13 @@ public class Order {
     @GeneratedValue
     private Long orderId;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "join_orders_carts",
-            joinColumns = {@JoinColumn(name = "order_id")},
-            inverseJoinColumns = {@JoinColumn(name = "cart_id")})
-    private List<Cart> carts = new ArrayList<>();
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "cartId")
+    private Cart cart;
 
-    @Column(name = "orderDate")
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
     private LocalDateTime orderDate;
-
-    @Column(name = "orderPaymentDate")
-    private LocalDateTime orderPaymentDate;
-
-    @Column(name = "orderValue")
-    private int orderValue;
-
-    @Column(name = "orderAmountPaid")
-    private int orderAmountPaid;
-
-    public Order(List<Cart> carts, LocalDateTime orderDate, LocalDateTime orderPaymentDate, int orderValue, int orderAmountPaid) {
-        this.carts = carts;
-        this.orderDate = ZonedDateTime.now().toLocalDateTime();
-        this.orderPaymentDate = orderPaymentDate;
-        this.orderValue = orderValue;
-        this.orderAmountPaid = orderAmountPaid;
-    }
 }
