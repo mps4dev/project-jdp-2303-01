d2c715eaef8c4877aec6f0091266664caace7776
Marcin Szuppe <mps4dev@gmail.com>
2023-03-22 18:08:37 +0100

    tmp1 test resolving conflicts

---
+++ src/test/java/com/kodilla/ecommercee/CartControllerTest.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CartControllerTest {
+
+    private final int x = 0;
+}
+
