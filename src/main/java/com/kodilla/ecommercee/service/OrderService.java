8d3a2630d5ab493941e6f5995180639d6b5ba2a1
domKul <114921142+domKul@users.noreply.github.com>
2023-03-22 11:27:58 +0100

    Update OrderService.java

--- src/main/java/com/kodilla/ecommercee/service/OrderService.java
+++ src/main/java/com/kodilla/ecommercee/service/OrderService.java
@@ -31,20 +31,4 @@ public class OrderService {
         Order order = new Order(null, cart, user);
         orderRepository.save(order);
     }
-
-    public List<Order> showOrders() {
-        return orderRepository.findAll();
-    }
-
-    public Order showOrder(final long orderId) throws OrderNotFoundException {
-        return orderRepository.findById(orderId).orElseThrow(OrderNotFoundException::new);
-    }
-
-    public Order saveOrder(final Order order) {
-        return orderRepository.save(order);
-    }
-
-    public void deleteOrder(final long orderId) {
-        orderRepository.deleteById(orderId);
-    }
 }
