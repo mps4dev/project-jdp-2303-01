86600f184b18e418d24d62a3f13a593ba89e1bc3
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-22 09:45:57 +0100

    poprawa formatowania i endpointa  createOrderFromCart

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -59,3 +59,4 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 }
+
--- src/main/java/com/kodilla/ecommercee/service/UserService.java
+++ src/main/java/com/kodilla/ecommercee/service/UserService.java
@@ -38,6 +38,7 @@ public class UserService {
         userRepository.save(user);
         return userKeyMapper.mapToUserKeyDTO(user.getUserKey());
     }
+
     public List<User> showUsers() {
         return userRepository.findAll();
     }
