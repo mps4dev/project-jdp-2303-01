2489199b8e524d9dee9f8b6923877f553392ad07
Paweł Staszewski <p.staszewski1@gmail.com>
2023-03-23 22:13:13 +0100

    adding class SecurityConfig

---
+++ src/main/java/com/kodilla/ecommercee/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.kodilla.ecommercee.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "v1/products").permitAll()
+                .antMatchers(HttpMethod.POST, "v1/products").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "v1/products").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "v1/products/{productId}").hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
+
