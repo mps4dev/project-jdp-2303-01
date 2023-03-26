decc28d5699e7334908929908ec4f45f4a62a7c3
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-03-26 23:52:11 +0200

    final commit

--- src/main/java/com/kodilla/ecommercee/service/OrderService.java
+++ src/main/java/com/kodilla/ecommercee/service/OrderService.java
@@ -25,12 +25,14 @@ public class OrderService {
     public List<Order> showOrders() {
         return orderRepository.findAll();
     }
+
     public Order showOrder(final long orderId) throws OrderNotFoundException {
         return orderRepository.findById(orderId).orElseThrow(OrderNotFoundException::new);
     }
     public Order saveOrder(final Order order) {
         return orderRepository.save(order);
     }
+
     public void deleteOrder(final long orderId) {
         orderRepository.deleteById(orderId);
     }
